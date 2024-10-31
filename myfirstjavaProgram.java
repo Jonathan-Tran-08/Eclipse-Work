63ff1364fa3911306ec969951e219399d730bef1
Jonathan-Tran-08 <jonathant4083@gmail.com>
2024-10-31 12:56:14 -0400

    Create myfirstjavaProgram.java

---
+++ myfirstjavaProgram.java
@@ -0,0 +1,20 @@
+package firstProject;
+
+public class myfirstjavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.print("what's nine ");
+		System.out.println("plus ");
+		System.out.println("ten/t ");
+		double x = 9.2;
+		int y = 10;
+		//casting
+		System.out.println((int)x+ " " + (double)y);*/
+		System.out.println("name\tage\theight");
+		System.out.println("Archie\t17\t5'9\"");
+		System.out.println("Betty\t17\t5'6\"");
+		System.out.println("Jughead\t16\t6'");
+	}
+
+}
