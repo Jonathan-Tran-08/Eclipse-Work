d8b2071fac5ed023f504c461e8ed5da38d5930ef
Jonathan-Tran-08 <jonathant4083@gmail.com>
2024-11-08 12:42:55 -0500

    Update APCalendarTester.java

--- APCalendarTester.java
+++ APCalendarTester.java
@@ -2,7 +2,9 @@ public class APCalendarTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		APCalendar highschool = new APCalendar();
+		
+		System.out.println(highschool.firstDayOfYear(2000));
 	}
 
 }
